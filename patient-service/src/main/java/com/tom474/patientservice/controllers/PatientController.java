c0cecfe18a5ee232baeadcf21aa14f0327f4ca04
trmc <trmc@netcompany.com>
2025-10-06 14:43:11 +0700

    fear: implement delete patient endpoint

--- patient-service/src/main/java/com/tom474/patientservice/controllers/PatientController.java
+++ patient-service/src/main/java/com/tom474/patientservice/controllers/PatientController.java
@@ -40,4 +40,10 @@ public class PatientController {
 
         return ResponseEntity.ok().body(patientResponseDTO);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- patient-service/src/main/java/com/tom474/patientservice/services/PatientService.java
+++ patient-service/src/main/java/com/tom474/patientservice/services/PatientService.java
@@ -55,4 +55,8 @@ public class PatientService {
 
         return PatientMapper.toDTO(updatedPatient);
     }
+
+    public void deletePatient(UUID id) {
+        patientRepository.deleteById(id);
+    }
 }
