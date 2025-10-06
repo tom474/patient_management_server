a7cd8416788b47f5bf0c24142b2ab30f335198fb
trmc <trmc@netcompany.com>
2025-10-06 09:47:40 +0700

    chore: initialize project

---
+++ patient-service/src/test/java/com/tom474/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom474.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
